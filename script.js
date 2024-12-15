dbdaba12eb0e041c663fae6d199022d6eb1cf605
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-12-16 00:01:04 +0700

    Task 3 lesson

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let headerLInk = document.getElementById("header-article");
-let contactInfo = document.getElementById("link to contacts");
+let contactInfo = document.getElementById("linkToContact");
 console.log(headerLInk);
 console.log(contactInfo);
 
-document.getElementById("header-article").innerText = "Все статьи (2)";
+document.getElementById("header-article").innerText = "Все статьи (1)";
+
+let contactBlock = document.getElementsByClassName("section contacs")[0];
+
+let articlesBlock = document.getElementsByClassName("section articles")[0];
+
+headerLInk.addEventListener("click", () => {
+  articlesBlock.scrollIntoView({ behavior: "smooth" });
+});
+
+contactInfo.addEventListener("click", () => {
+  contactBlock.scrollIntoView({ behavior: "smooth" });
+});
