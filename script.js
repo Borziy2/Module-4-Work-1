253e5e0ac26566715ed5973c450a5da2c5b36496
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-12-19 23:08:30 +0700

    Task 5 Lesson

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { articleElements } from "./articles.js";
+
 let headerLInk = document.getElementById("header-article");
 let contactInfo = document.getElementById("link-to-contact");
 console.log(headerLInk);
@@ -17,40 +19,6 @@ contactInfo.addEventListener("click", () => {
   contactBlock.scrollIntoView({ behavior: "smooth" });
 });
 
-let articles = [
-  {
-    title: "почему я выбрал JavaScript",
-    text: "В этой статье я хочу расказать почему програмирую",
-    link: "javascript.html",
-  },
-  {
-    title: "Технологии,которые я применяю",
-    text: "В этой статье я хочу расказаваю про свои технологии",
-    link: "technology.html",
-  },
-  {
-    title: "GIT",
-    text: "В этой статье я раскажу почуму я использую GIT",
-    link: "git.html",
-  },
-];
 
-let articleElements = articles.map((article) => {
-  let articleEl = document.createElement("article");
-  articleEl.classList.add("section-element");
-  let titleEl = document.createElement("h3");
-  titleEl.innerText = article.title;
-  articleEl.appendChild(titleEl);
-
-  let textEl = document.createElement("p");
-  textEl.innerText = article.text;
-  articleEl.appendChild(textEl);
-
-  let linkEl = document.createElement("a");
-  linkEl.innerText = article.link;
-  linkEl.href = article.link;
-  linkEl.innerText = "Читать";
-  articleEl.appendChild(linkEl);
-  return articleEl;
-});
-articlesBlock.append(...articleElements);
+let articleElementsnew = articleElements();
+articlesBlock.append(...articleElementsnew);
